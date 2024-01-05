49eaf5d424b054eafb19415df5582f5ae415122a
Nwe Yi Aung <nweyiaung1990@gmail.com>
2024-01-05 15:20:07 +0630

    first commit

---
+++ packages/authentication_repository.dart
@@ -0,0 +1,13 @@
+import 'dart:async';
+
+enum AuthenticationStatus { unknown, authenticated, unauthenticated }
+
+class AuthenticationRepository {
+  final _controller = StreamController<AuthenticationStatus>();
+
+  Stream<AuthenticationStatus> get status async* {
+    await Future.delayed(const Duration(seconds: 1));
+    yield AuthenticationStatus.unauthenticated;
+    yield* _controller.stream;
+  }
+}
