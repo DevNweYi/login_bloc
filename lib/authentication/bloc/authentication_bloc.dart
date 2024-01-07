48e72c5936966e17e9c41a4515767256a1c97a68
DevNweYi <nweyiaung610@gmail.com>
2024-01-07 21:44:53 +0630

    implementing code

---
+++ lib/authentication/bloc/authentication_bloc.dart
@@ -0,0 +1,27 @@
+import 'dart:async';
+
+import 'package:authentication_repository/authentication_repository.dart';
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:user_repository/user_repository.dart';
+
+part 'authentication_event.dart';
+part 'authentication_state.dart';
+
+class AuthenticationBloc
+    extends Bloc<AuthenticationEvent, AuthenticationState> {
+  final AuthenticationRepository _authenticationRepository;
+  final UserRepository _userRepository;
+  late StreamSubscription<AuthenticationStatus>
+      _authenticationStatusSubscription;
+  AuthenticationBloc(
+      {required AuthenticationRepository authenticationRepository,
+      required UserRepository userRepository})
+      : _authenticationRepository = authenticationRepository,
+        _userRepository = userRepository,
+        super(const AuthenticationState.unknown()) {
+    on<AuthenticationEvent>((event, emit) {
+      // TODO: implement event handler
+    });
+  }
+}
